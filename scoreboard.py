a23dbe7b5ad4d6763f7a3e1e0f263676c07f8e64
zwsnider <zwsnider@yahoo.com>
2020-03-09 01:36:42 -0500

    display number of ships

--- scoreboard.py
+++ scoreboard.py
@@ -1,10 +1,14 @@
 import pygame.font
+from pygame.sprite import Group
+
+from ship import Ship
 
 class Scoreboard:
     """A class to report scoring information."""
 
     def __init__(self, ai_game):
         """Initialize scorekeeping attrbutes."""
+        self.ai_game = ai_game
         self.screen = ai_game.screen
         self.screen_rect = self.screen.get_rect()
         self.settings = ai_game.settings
@@ -18,6 +22,16 @@ class Scoreboard:
         self.prep_score()
         self.prep_high_score()
         self.prep_level()
+        self.prep_ships()
+
+    def prep_ships(self):
+        """Show how many ships are left."""
+        self.ships = Group()
+        for ship_number in range(self.stats.ships_left):
+            ship = Ship(self.ai_game)
+            ship.rect.x = 10 + ship_number * ship.rect.width
+            ship.rect.y = 10
+            self.ships.add(ship)
 
     def prep_score(self):
         """Turn the score into a rendered image."""
@@ -52,10 +66,11 @@ class Scoreboard:
         self.level_rect.top = self.score_rect.bottom + 10
 
     def show_score(self):
-        """Draw score to the screen."""
+        """Draw scores, level, and ships to the screen."""
         self.screen.blit(self.score_image, self.score_rect)
         self.screen.blit(self.high_score_image, self.high_score_rect)
         self.screen.blit(self.level_image, self.level_rect)
+        self.ships.draw(self.screen)
 
     def check_high_score(self):
         """Check to see if there's a new high score."""
