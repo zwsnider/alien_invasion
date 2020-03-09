da87ee7571555140e3bf074d921ef14f4f0a08d5
zwsnider <zwsnider@yahoo.com>
2020-03-08 23:31:42 -0500

    bullet size test

--- settings.py
+++ settings.py
@@ -13,10 +13,13 @@ class Settings:
 
         # Bullet settings
         self.bullet_speed = 1.0
-        self.bullet_width = 3
+        self.bullet_width = 3000
         self.bullet_height = 15
         self.bullet_color = (60,60,60)
         self.bullets_allowed = 3
 
         # Alien settings
         self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        # fleet_direction oof 1 represents right; -1 represents left.
+        self.fleet_direction = 1
