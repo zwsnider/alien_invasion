f12bcd7186404200bcf2df3160cd1a2f95285f8c
zwsnider <zwsnider@yahoo.com>
2020-03-08 23:30:12 -0500

    detect bullet collision

--- alien_invasion.py
+++ alien_invasion.py
@@ -93,6 +93,10 @@ class AlienInvasion:
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
 
+        # Check for any bullets that have hit aliens.
+        #   If so, get rid of the bullet and the alien.
+        collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
+
     def _check_events(self):
         """Respond to keypresses and mouse events."""
         for event in pygame.event.get():
