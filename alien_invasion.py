b4cb6042d24ede147a014d8d975b7f64fed2a50d
zwsnider <zwsnider@yahoo.com>
2020-03-08 23:35:04 -0500

    repopulate fleet

--- alien_invasion.py
+++ alien_invasion.py
@@ -97,6 +97,11 @@ class AlienInvasion:
         #   If so, get rid of the bullet and the alien.
         collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
 
+        if not self.aliens:
+            # Destroy existing bullets and create a new fleet.
+            self.bullets.empty()
+            self._create_fleet()
+
     def _check_events(self):
         """Respond to keypresses and mouse events."""
         for event in pygame.event.get():
