9d00c52ca91f4696db8e148f8d66590d7d6d4ce2
zwsnider <zwsnider@yahoo.com>
2020-03-08 22:32:06 -0500

    Firing bullets syntax fix

--- alien_invasion.py
+++ alien_invasion.py
@@ -51,7 +51,7 @@ class AlienInvasion:
             self.ship.moving_left = True
         elif event.key == pygame.K_q:
             sys.exit()
-        elif event.key = pygame.K_SPACE:
+        elif event.key == pygame.K_SPACE:
             self._fire_bullet()
 
     def _check_keyup_events(self,event):
